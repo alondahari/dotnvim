9a242a9eae68926caca1e54e9e6b5a60d76ffac1
Alon Dahari <alon@uptechstudio.com>
2023-06-07 10:43:54 -0700

    Add keymaps from https://github.com/ThePrimeagen
    <!-- ps-id: a55ddbc8-0dd8-47be-8fe4-f9c6a32867b9 -->

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -32,19 +32,19 @@ vim.keymap.set('n', '<leader>xt', '<cmd>bd<cr>') -- close this buffer
 vim.keymap.set('n', '<leader>xa', '<cmd>%bd<cr>') -- close all buffers
 
 -- Trouble
-vim.keymap.set("n", "<leader>tt", "<cmd>TroubleToggle<cr>",
+vim.keymap.set("n", "<leader>it", "<cmd>TroubleToggle<cr>",
   {silent = true, noremap = true}
 )
-vim.keymap.set("n", "<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+vim.keymap.set("n", "<leader>iw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
   {silent = true, noremap = true}
 )
-vim.keymap.set("n", "<leader>td", "<cmd>TroubleToggle document_diagnostics<cr>",
+vim.keymap.set("n", "<leader>id", "<cmd>TroubleToggle document_diagnostics<cr>",
   {silent = true, noremap = true}
 )
-vim.keymap.set("n", "<leader>tl", "<cmd>TroubleToggle loclist<cr>",
+vim.keymap.set("n", "<leader>il", "<cmd>TroubleToggle loclist<cr>",
   {silent = true, noremap = true}
 )
-vim.keymap.set("n", "<leader>tq", "<cmd>TroubleToggle quickfix<cr>",
+vim.keymap.set("n", "<leader>iq", "<cmd>TroubleToggle quickfix<cr>",
   {silent = true, noremap = true}
 )
 vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
@@ -57,3 +57,46 @@ end)
 -- Add new lines
 vim.keymap.set('n', '<Enter>', 'o<ESC>', {silent = true})
 vim.keymap.set('n', '<S-Enter>', 'O<ESC>', {silent = true})
+
+-- tree view
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- move selected lines up / down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- tack next line onto this one without moving the cursor
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- move half page down without moving cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- This is going to get me cancelled
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "<Space>g", vim.cmd.Git)
+
+-- navigate errors
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- replace all occurences of current word in file
+vim.keymap.set("n", "<leader>ra", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
