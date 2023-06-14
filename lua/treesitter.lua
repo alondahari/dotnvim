49179fb3540801886b5e43741f5176cfd82ee0da
Alon Dahari <alon@uptechstudio.com>
2023-06-14 14:43:02 +0100

    Disable dart indent
    Temporary fix for issue https://github.com/nvim-treesitter/nvim-treesitter/issues/4945

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -5,5 +5,5 @@ local ts = require('nvim-treesitter.configs')
 ts.setup {
   ensure_installed = { "c", "lua", "luadoc", "markdown", "markdown_inline", "rust", "toml", "sql", "swift", "yaml", "terraform", "typescript", "vim", "query", "bash", "css", "scss", "dart", "diff", "dockerfile", "git_rebase", "gitattributes", "gitignore", "graphql", "html", "javascript", "jsdoc", "json" },
   highlight = { enable = true },
-  indent = { enable = true },
+  indent = { enable = true, disable = { "dart" } },
 }
