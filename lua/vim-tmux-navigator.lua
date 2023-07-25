ba3fa012936ab005ce1d68205ccbec6cd01c84fc
Alon Dahari <alon@uptechstudio.com>
2023-07-25 18:21:49 +0100

    Update navigator config for new keyboard

--- lua/vim-tmux-navigator.lua
+++ lua/vim-tmux-navigator.lua
@@ -1,7 +1,7 @@
 vim.g.tmux_navigator_no_mappings = '1'
 
-vim.keymap.set("n", "<C-Left>", "<cmd>TmuxNavigateLeft<cr>")
-vim.keymap.set("n", "<C-Down>", "<cmd>TmuxNavigateDown<cr>")
-vim.keymap.set("n", "<C-Up>", "<cmd>TmuxNavigateUp<cr>")
-vim.keymap.set("n", "<C-Right>", "<cmd>TmuxNavigateRight<cr>")
+vim.keymap.set("n", "<C-n>", "<cmd>TmuxNavigateLeft<cr>")
+vim.keymap.set("n", "<C-e>", "<cmd>TmuxNavigateDown<cr>")
+vim.keymap.set("n", "<C-u>", "<cmd>TmuxNavigateUp<cr>")
+vim.keymap.set("n", "<C-i>", "<cmd>TmuxNavigateRight<cr>")
 vim.keymap.set("n", "<C-/>", "<cmd>TmuxNavigatePrevious<cr>")
