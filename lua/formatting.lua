c0b81f11f694bdcb1ae9eef22a49ba3507522022
Alon Dahari <alon@uptechstudio.com>
2023-08-14 15:06:57 +0100

    Fix some formatting issue with new version

--- lua/formatting.lua
+++ lua/formatting.lua
@@ -1,2 +1,5 @@
-vim.g.neoformat_enabled_javascript = "['prettierd', 'eslint_d']"
+vim.g.neoformat_enabled_cpp = { "uncrustify" }
+vim.g.neoformat_enabled_javascript = { "prettierd", "eslint_d" }
+vim.g.neoformat_enabled_typescript = { "prettierd", "eslint_d" }
+
 vim.g.neoformat_run_all_formatters = 1
