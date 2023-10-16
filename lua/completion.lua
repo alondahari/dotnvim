a73927218bff79b3f76f81379477132fd6da8de2
Alon Dahari <alon@uptechstudio.com>
2023-10-16 23:23:15 +0100

    Finally stop hijacking CR for completion
    This was super annoying especially when writing commit messages

--- lua/completion.lua
+++ lua/completion.lua
@@ -6,13 +6,13 @@ local lspkind = require('lspkind')
 
 cmp.setup({
   mapping = {
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<Up>'] = cmp.mapping.select_prev_item(),
+    ['<Down>'] = cmp.mapping.select_next_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs( -4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
+    ['<C-i>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Insert,
       select = true
     }),
